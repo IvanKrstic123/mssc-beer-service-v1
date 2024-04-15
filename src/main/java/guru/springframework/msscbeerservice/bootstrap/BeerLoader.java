a5c017cc41811a641d8709d730d6041c912535e8
Ivan Krstic <krstic8687@yahoo.com>
2024-04-15 14:09:37 +0200

    database initialization and hibernate dialect fix

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -14,9 +14,9 @@ import java.util.UUID;
 @Component
 public class BeerLoader implements CommandLineRunner {
 
-    public static final String BEER_1_UPC = "0631234200036";
-    public static final String BEER_2_UPC = "0631234300019";
-    public static final String BEER_3_UPC = "0083783375213";
+    public static final String BEER_1_UPC = "06387803414";
+    public static final String BEER_2_UPC = "06254587821";
+    public static final String BEER_3_UPC = "0614789785";
 
     public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
 
